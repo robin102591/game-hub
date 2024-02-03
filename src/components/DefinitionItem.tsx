cb0216e976077227aeab6da0e294c4edc4ceef18
robin102591 <robin.nobleza@gmail.com>
2024-02-04 00:02:21 +0800

    Build game attribute

---
+++ src/components/DefinitionItem.tsx
@@ -0,0 +1,22 @@
+import { Box, Heading } from "@chakra-ui/react";
+import React, { ReactNode } from "react";
+
+interface DefinitionItemProps {
+  term: string;
+  children: ReactNode | ReactNode[];
+}
+
+const DefinitionItem = ({ term, children }: DefinitionItemProps) => {
+  return (
+    <>
+      <Box marginY={5}>
+        <Heading as="dt" fontSize="md" color="gray.600">
+          {term}
+        </Heading>
+        <dd>{children}</dd>
+      </Box>
+    </>
+  );
+};
+
+export default DefinitionItem;
