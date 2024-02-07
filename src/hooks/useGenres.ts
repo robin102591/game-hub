2df29a84898d03a9463dfa81015496109b83d447
robin102591 <robin.nobleza@gmail.com>
2024-02-07 23:47:02 +0800

    Refactor entity interfaces.

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import ms from 'ms';
-import { Genre } from '../entities/Genre';
+import Genre from '../entities/Genre';
 import APIClient from '../services/api-client';
 
 const apiClient = new APIClient<Genre>('/genres')
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import ms from 'ms';
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents')
